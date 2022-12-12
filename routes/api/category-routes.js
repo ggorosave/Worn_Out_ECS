ab77e75e21896e2f23b9600db234c77cada06253
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-12-12 16:45:14 -0700

    Added category route to find all data

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,26 +3,58 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+
+    const categoryData = await Category.findAll({
+      // includes the product data with category
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+
+    const categoryData = await Category.findByPk({})
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
