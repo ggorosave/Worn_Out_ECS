aa79c087dd3bc7554a5053c77d4a947a0f5182b9
ggorosave <103039442+ggorosave@users.noreply.github.com>
2022-12-14 16:04:44 -0700

    Added comments to connection file

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,16 @@ require('dotenv').config();
 // imports sequelize
 const Sequelize = require('sequelize');
 
+// checks if connect to heroku jawsdb and uses local host if not
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+    host: 'localhost',
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
 
+// exports connectiongit 
 module.exports = sequelize;
